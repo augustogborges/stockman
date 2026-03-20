ac4089679d8bcb24f2776257d3bdef50542b2c48
seraph036 <alexandre.hipolito@proton.me>
2026-03-20 20:22:51 -0300

    fix writeDB to accept existing data

--- writeDb.py
+++ writeDb.py
@@ -8,6 +8,12 @@ quant = input("Digite a quantidade do item: ")
 
 dados = {}
 
+with open("dB.json", "r", encoding="utf-8") as dadosExist:
+    try:
+        dados = json.load(dadosExist)
+    except:
+        dados = {}
+
 dados.update({item: quant})
 
 # salvar no arquivo JSON
