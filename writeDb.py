4829f47a053afbcaa6f431be94e524e7b497c86a
Augusto <augusto.guimaraesb123@gmail.com>
2026-03-20 20:18:18 -0300

    add writeDb.py

---
+++ writeDb.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+#Entrada de dados
+
+item = input("Digite o item: ")
+quant = input("Digite a quantidade do item: ")
+
+dados = {}
+
+dados.update({item: quant})
+
+# salvar no arquivo JSON
+with open("dB.json", "w", encoding="utf-8") as itens:
+    json.dump(dados, itens, indent=4, ensure_ascii=False)
+
+print("Dados salvos com sucesso!")
