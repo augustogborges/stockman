a0e5fb9f0a268867b196e5af916538a58989ae03
seraph036 <alexandre.hipolito@proton.me>
2026-03-20 19:46:14 -0300

    primeiros arquivos da UI

---
+++ main.py
@@ -0,0 +1,18 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from pathlib import Path
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtCore import Slot
+
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.addImportPath(Path(__file__).parent)
+    engine.loadFromModule("stockman_py", "Main")
+    if not engine.rootObjects():
+        sys.exit(-1)
+    sys.exit(app.exec())
